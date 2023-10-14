2788130161d3697131c79c71f2b0fc1d45dee95e
ipetkov <afew@abv.bg>
2023-10-14 10:45:43 +0300

    Added dependency for the error message related to the SLF4J. I hate that error message. It makes our work less formal. Added RESTConnectionControllerTests Added the necessary tests to check whether user is able to send connection request, approve and disconnect. Added RESTSkillControllerTests Added the necessary tests to check whether user is able to find skills/services, select and deselect.

--- src/test/java/test/cases/wearerestassured/tests/RESTConnectionControllerTest.java
+++ src/test/java/test/cases/wearerestassured/tests/RESTConnectionControllerTest.java
@@ -1,7 +1,87 @@
 package test.cases.wearerestassured.tests;
 
+import com.telerikacademy.testframework.models.RequestModel;
+import com.telerikacademy.testframework.models.UserModel;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
 import test.cases.BaseTestSetup;
 
+import static com.telerikacademy.testframework.utils.UserRoles.ROLE_USER;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.testng.Assert.*;
 public class RESTConnectionControllerTest extends BaseTestSetup {
 
+
+    @Test
+    public void userCanSendRequestToAnotherUser() {
+
+        UserModel sender = weareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = weareApi.registerUser(ROLE_USER.toString());
+
+        RequestModel request = weareApi.sendRequest(sender, receiver);
+
+        assertEquals(request.getSender().getId(), sender.getId(), "Sender doesn't match the one in the request.");
+        assertEquals(request.getReceiver().getId(), receiver.getId(), "Receiver doesn't match the one in the request.");
+
+        weareApi.disableUser(globalAdminUser, sender.getId());
+        weareApi.disableUser(globalAdminUser, receiver.getId());
+    }
+
+    @Test
+    public void userCanApproveRequestFromAnotherUser() {
+
+        UserModel sender = weareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = weareApi.registerUser(ROLE_USER.toString());
+
+        RequestModel sentRequest = weareApi.sendRequest(sender, receiver);
+
+        Response approveRequestResponse = weareApi.approveRequest(receiver, sentRequest);
+
+        assertEquals(approveRequestResponse.body().asString(), String.format("%s approved request of %s",
+                receiver.getUsername(), sender.getUsername()), "Request is not approved.");
+
+        weareApi.disableUser(globalAdminUser, sender.getId());
+        weareApi.disableUser(globalAdminUser, receiver.getId());
+    }
+
+    @Test
+    public void userCanDisconnectFromAnotherUser() {
+
+        UserModel sender = weareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = weareApi.registerUser(ROLE_USER.toString());
+
+        RequestModel sendRequest = weareApi.sendRequest(sender, receiver);
+
+        weareApi.approveRequest(receiver, sendRequest);
+
+        Response disconnectRequestResponse = weareApi.disconnectFromUser(receiver, sender);
+
+        int statusCode = disconnectRequestResponse.getStatusCode();
+        assertEquals(statusCode, SC_OK, "Incorrect status code. Expected 200.");
+        assertEquals(disconnectRequestResponse.body().asString(), String.format("%s disconnected from %s",
+                receiver.getUsername(), sender.getUsername()), "Disconnection was not done");
+
+        weareApi.disableUser(globalAdminUser, sender.getId());
+        weareApi.disableUser(globalAdminUser, receiver.getId());
+    }
+
+    @Test
+    public void userCanGetUserRequests() {
+
+        UserModel sender = weareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = weareApi.registerUser(ROLE_USER.toString());
+
+        weareApi.sendRequest(sender, receiver);
+
+        RequestModel[] requests = weareApi.getUserRequests(receiver);
+
+        assertTrue(requests.length > 0, "There are no requests");
+        for (RequestModel request : requests) {
+            assertEquals(request.getClass(), RequestModel.class, "Wrong type of request");
+            assertNotNull(request, "Request is null");
+        }
+
+        weareApi.disableUser(globalAdminUser, sender.getId());
+        weareApi.disableUser(globalAdminUser, receiver.getId());
+    }
 }
--- src/test/java/test/cases/wearerestassured/tests/RESTSkillControllerTest.java
+++ src/test/java/test/cases/wearerestassured/tests/RESTSkillControllerTest.java
@@ -1,7 +1,99 @@
 package test.cases.wearerestassured.tests;
 
+import com.telerikacademy.testframework.models.SkillModel;
+import com.telerikacademy.testframework.models.UserModel;
+import io.restassured.response.Response;
+import org.testng.annotations.*;
 import test.cases.BaseTestSetup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.telerikacademy.testframework.utils.UserRoles.*;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.testng.Assert.*;
+
 public class RESTSkillControllerTest extends BaseTestSetup {
+
+    private UserModel skillUser;
+
+    @BeforeClass
+    private void setUp() {
+        skillUser = weareApi.registerUser(ROLE_USER.toString());
+    }
+
+    @AfterClass
+    public void clear() {
+        weareApi.disableUser(globalAdminUser, skillUser.getId());
+    }
+
+    @Test
+    public void userCanFindAllSkills() {
+
+        List<Integer> skillIds = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            SkillModel skill = weareApi.createSkill(skillUser);
+            skillIds.add(skill.getSkillId());
+        }
+
+        SkillModel[] foundSkills = weareApi.getAllSkills();
+
+        assertTrue(foundSkills.length > 0, "There are no skills found.");
+
+        for (SkillModel skill : foundSkills) {
+            if (skillIds.contains(skill.getSkillId())) {
+                weareApi.deleteSkill(skill.getSkillId());
+            }
+        }
+
+    }
+
+    @Test
+    public void userCanCreateSkill() {
+
+        SkillModel skill = weareApi.createSkill(skillUser);
+
+        assertNotNull(skill, "Skill was not created.");
+
+        weareApi.deleteSkill(skill.getSkillId());
+    }
+
+    @Test
+    public void userCanDeleteSkill() {
+
+        SkillModel skill = weareApi.createSkill(skillUser);
+
+        weareApi.deleteSkill(skill.getSkillId());
+
+        assertFalse(weareApi.skillExists(skill.getSkillId()), "Skill was not deleted.");
+
+    }
+
+    @Test
+    public void userCanEditSkill() {
+
+        SkillModel skill = weareApi.createSkill(skillUser);
+
+        Response editedSkillResponse = weareApi.editSkill(skill.getSkillId());
+
+        int statusCode = editedSkillResponse.getStatusCode();
+        assertEquals(statusCode, SC_OK, "Incorrect status code. Expected 200.");
+
+        weareApi.deleteSkill(skill.getSkillId());
+    }
+
+    @Test
+    public void userCanGetSkillById() {
+
+        SkillModel skill = weareApi.createSkill(skillUser);
+
+        SkillModel foundSkill = weareApi.getSkillById(skill.getSkillId());
+
+        assertEquals(foundSkill.getSkillId(), skill.getSkillId(), "Ids do not match.");
+        assertNotNull(foundSkill, "Skill not found.");
+
+        weareApi.deleteSkill(skill.getSkillId());
+
+    }
 
 }
