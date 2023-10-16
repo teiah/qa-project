073de5772a0aaa08d54ac003d702cafb88698eb9
Teodora Hristova <teodoravhristova@gmail.com>
2023-10-16 13:21:18 +0300

    add: use clickElementWithJavaScript() in deletePost()

--- src/main/java/com/telerikacademy/testframework/pages/weare/DeletePostPage.java
+++ src/main/java/com/telerikacademy/testframework/pages/weare/DeletePostPage.java
@@ -1,19 +1,20 @@
 package com.telerikacademy.testframework.pages.weare;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 public class DeletePostPage extends BaseWearePage {
 
     public DeletePostPage(WebDriver driver, Integer postId) {
-        super(driver, "weare.postPage", postId);
+        super(driver, "weare.deletePostPage", postId);
     }
 
     public void deletePost() {
-
+        actions.scrollToElement("weare.deletePostPage.confirmationSelector");
         actions.waitForElementClickable("weare.deletePostPage.confirmationSelector");
-        actions.clickElement("weare.deletePostPage.confirmationSelector");
+        actions.clickElementWithJavascript("weare.deletePostPage.confirmationSelector");
+        actions.waitForElementClickable("weare.deletePostPage.deleteOption");
         actions.clickElement("weare.deletePostPage.deleteOption");
         actions.waitForElementClickable("weare.deletePostPage.submitButton");
-        actions.clickElement("weare.deletePostPage.submitButton");
+        actions.clickElementWithJavascript("weare.deletePostPage.submitButton");
     }
 }
