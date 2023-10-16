0750b96d86f2557bdbb0c8a0871891ab6de8565f
ipetkov <afew@abv.bg>
2023-10-17 00:47:17 +0300

    removed _ from REST Assured tests

--- src/test/java/test/cases/wearerestassured/tests/tests/RESTConnectionControllerTest.java
+++ src/test/java/test/cases/wearerestassured/tests/tests/RESTConnectionControllerTest.java
@@ -14,8 +14,8 @@ public class RESTConnectionControllerTest extends BaseWeareRestAssuredTest {
     @Test
     public void userCanSendRequestToAnotherUser() {
 
-        UserModel sender = WEareApi.registerUser(ROLEUSER.toString());
-        UserModel receiver = WEareApi.registerUser(ROLEUSER.toString());
+        UserModel sender = WEareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = WEareApi.registerUser(ROLE_USER.toString());
 
         RequestModel[] requests = WEareApi.getUserRequests(receiver);
         int previousRequestsCount = requests.length;
@@ -36,8 +36,8 @@ public class RESTConnectionControllerTest extends BaseWeareRestAssuredTest {
     @Test
     public void userCanApproveRequestFromAnotherUser() {
 
-        UserModel sender = WEareApi.registerUser(ROLEUSER.toString());
-        UserModel receiver = WEareApi.registerUser(ROLEUSER.toString());
+        UserModel sender = WEareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = WEareApi.registerUser(ROLE_USER.toString());
 
         RequestModel[] requests = WEareApi.getUserRequests(receiver);
         int previousRequestsCount = requests.length;
@@ -66,8 +66,8 @@ public class RESTConnectionControllerTest extends BaseWeareRestAssuredTest {
     @Test
     public void userCanDisconnectFromAnotherUser() {
 
-        UserModel sender = WEareApi.registerUser(ROLEUSER.toString());
-        UserModel receiver = WEareApi.registerUser(ROLEUSER.toString());
+        UserModel sender = WEareApi.registerUser(ROLE_USER.toString());
+        UserModel receiver = WEareApi.registerUser(ROLE_USER.toString());
 
         RequestModel sendRequest = WEareApi.sendRequest(sender, receiver);
 
