62b807e22c6e79d8c07f0a27c76358b746046420
Teodora Hristova <teodoravhristova@gmail.com>
2023-10-18 18:43:23 +0300

    add: extractPostId() and cleanUp() for posts tests

--- src/main/java/com/telerikacademy/testframework/pages/weare/LatestPostsPage.java
+++ src/main/java/com/telerikacademy/testframework/pages/weare/LatestPostsPage.java
@@ -2,6 +2,11 @@ package com.telerikacademy.testframework.pages.weare;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.telerikacademy.testframework.utils.Utils.getUIMappingByKey;
+
 public class LatestPostsPage extends BaseWearePage {
 
     public LatestPostsPage(WebDriver driver) {
@@ -29,5 +34,23 @@ public class LatestPostsPage extends BaseWearePage {
     public void explorePost(Integer postId) {
         actions.waitForElementVisible("weare.latestPostPage.exploreThisPostButton", postId.toString());
         actions.clickElement("weare.latestPostPage.exploreThisPostButton", postId.toString());
+    }
+
+
+    public Integer extractPostId() {
+
+        actions.waitForElementClickable("weare.latestPostPage.exploreThisPostButtonFirst");
+        String attributeName = "href";
+        String hrefValue = actions.getElementAttribute(
+                getUIMappingByKey("weare.latestPostPage.exploreThisPostButtonFirst"), attributeName);
+
+        Pattern pattern = Pattern.compile("/posts/(\\d+)$");
+        Matcher matcher = pattern.matcher(hrefValue);
+
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(1));
+        } else {
+            return null;
+        }
     }
 }
--- src/test/java/test/cases/weareseleniumtests/tests/SeleniumPostTest.java
+++ src/test/java/test/cases/weareseleniumtests/tests/SeleniumPostTest.java
@@ -1,12 +1,34 @@
 package test.cases.weareseleniumtests.tests;
 
 import api.models.PostModel;
+import api.models.UserModel;
 import com.telerikacademy.testframework.pages.weare.*;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import test.cases.weareseleniumtests.base.BaseWeareSeleniumTest;
 
+import static com.telerikacademy.testframework.utils.UserRoles.ROLE_ADMIN;
+
 public class SeleniumPostTest extends BaseWeareSeleniumTest {
+
+    // Post to be cleaned after each test
+    private Integer postId;
+
+    @AfterMethod
+    public void cleanUp(ITestResult result) {
+        HomePage homePage = new HomePage(actions.getDriver());
+        homePage.navigateToPage();
+        if (homePage.isLoggedIn()) {
+            homePage.logout();
+        }
+
+        if (postId != null) {
+            WEareApi.deletePost(globalUser, postId);
+            postId = null;
+        }
+    }
 
     @Test
     // only text, default visibility private, no image
@@ -25,14 +47,21 @@ public class SeleniumPostTest extends BaseWeareSeleniumTest {
         LatestPostsPage latestPostsPage = new LatestPostsPage(actions.getDriver());
         latestPostsPage.assertPageNavigated();
         latestPostsPage.assertPostIsCreated(postMessage);
+
+        postId = latestPostsPage.extractPostId();
+
+        Assert.assertNotNull(postId, "Unable to find latest post id");
+
+        PostPage postPage = new PostPage(actions.getDriver(), postId);
+        postPage.navigateToPage();
+        postPage.assertPageNavigated();
     }
 
     @Test
     public void PostLiked_By_User() {
 
-        boolean publicVisibility = true;
-        PostModel createdPost = this.WEareApi.createPost(globalUser, publicVisibility);
-        Integer postId = createdPost.getPostId();
+        PostModel createdPost = WEareApi.createPost(globalUser, true);
+        postId = createdPost.getPostId();
 
         LoginPage loginPage = new LoginPage(actions.getDriver());
         loginPage.loginUser(globalUserUsername, globalUserPassword);
@@ -48,16 +77,15 @@ public class SeleniumPostTest extends BaseWeareSeleniumTest {
     @Test
     public void PostEdited_By_AdminUser_When_NotAuthor() {
 
-        boolean publicVisibility = true;
-        PostModel createdPost = this.WEareApi.createPost(globalUser, publicVisibility);
-        Integer postId = createdPost.getPostId();
+        PostModel createdPost = this.WEareApi.createPost(globalUser, true);
+        postId = createdPost.getPostId();
 
         LoginPage loginPage = new LoginPage(actions.getDriver());
         loginPage.loginUser(adminUsername, adminPassword);
 
         EditPostPage editPostPage = new EditPostPage(actions.getDriver(), postId);
         editPostPage.navigateToPage();
-        String message = "vse taq";
+        String message = helpers.generatePostContent();
         editPostPage.editPostVisibility();
         editPostPage.editPostMessage(message);
         editPostPage.savePostChanges();
@@ -69,8 +97,7 @@ public class SeleniumPostTest extends BaseWeareSeleniumTest {
     @Test
     public void PostDeleted_By_AdminUser_When_NotAuthor() {
 
-        boolean publicVisibility = true;
-        PostModel createdPost = this.WEareApi.createPost(globalUser, publicVisibility);
+        PostModel createdPost = this.WEareApi.createPost(globalUser, true);
         Integer postId = createdPost.getPostId();
 
         LoginPage loginPage = new LoginPage(actions.getDriver());
