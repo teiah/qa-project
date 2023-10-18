6dee7d25898c59f1fab29f4c6f4974511bad3ed6
Ivailo Tabakov <ivo.tabakov@gmail.com>
2023-10-18 11:47:38 +0300

    Base url

--- src/main/java/com/telerikacademy/testframework/utils/Constants.java
+++ src/main/java/com/telerikacademy/testframework/utils/Constants.java
@@ -2,8 +2,8 @@ package com.telerikacademy.testframework.utils;
 
 public class Constants {
 
-//    public static final String BASE_URL = "http://localhost:8081";
+    public static final String BASE_URL = "http://localhost:8081";
     public static final String API = "/api";
-    public static final String BASE_URL = "https://enigmatic-scrubland-41901-78317c7c2a95.herokuapp.com";
+//    public static final String BASE_URL = "https://enigmatic-scrubland-41901-78317c7c2a95.herokuapp.com";
 
 }
