6983aa96d1b59c3411e87ba1c30ffc7dd9f49404
Ivailo Tabakov <ivo.tabakov@gmail.com>
2023-10-19 09:12:25 +0300

    Base url change

--- src/main/java/com/telerikacademy/testframework/utils/Constants.java
+++ src/main/java/com/telerikacademy/testframework/utils/Constants.java
@@ -2,9 +2,9 @@ package com.telerikacademy.testframework.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://localhost:8081";
+//    public static final String BASE_URL = "http://localhost:8081";
     public static final String API = "/api";
 //    public static final String BASE_URL = "https://aqueous-tor-48457-c91497a4c116.herokuapp.com";
-//    public static final String BASE_URL = "http://84.21.205.241:8081";
+    public static final String BASE_URL = "http://84.21.205.241:8081";
 
 }
